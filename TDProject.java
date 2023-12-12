97f8adfded5bced773021a306c0a9075cc1444d1
rodger2003 <48264113+dani0303@users.noreply.github.com>
2023-12-11 21:23:37 -0500

    Update TDProject.java

--- TDProject.java
+++ TDProject.java
@@ -1,32 +1,61 @@
 package utils;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 public class TDProject {
-    private Map<BusinessName, String> directory;
-    private Map<String, BusinessName> reverseDirectory;
+    private SortedArrayDictionary <BusinessName, String> directory;
+    private SortedArrayDictionary <BusinessName, String> reverseDirectory;
 
     public TDProject() {
-        this.directory = new TreeMap<>();
-        this.reverseDirectory = new TreeMap<>();
+        this.directory = new SortedArrayDictionary<>();
+        
     }
 
-    public void readFile(String fileName) {
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(",");
-                if (parts.length == 2) {
-                    BusinessName businessName = new BusinessName(parts[0].trim(), parts[1].trim());
-                    directory.put(businessName, parts[2].trim());
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    public boolean readFile(String fileName){
+        File datafile = new File (fileName);
+        String line;
+        BusinessName name; 
+        String phoneNumber;
+        try {
+        	Scanner read = new Scanner (datafile);
+        	while (read.hasNext()) {
+        		line = read.nextLine().trim();
+        		int lastBlank = line.lastIndexOf(' ');
+        		name = new BusinessName(line.substring(0, lastBlank).trim());
+        		phoneNumber = line.substring(lastBlank+1);
+        		directory.add(name, phoneNumber);
+        		if (line.isEmpty())
+        			continue;   	
+        	}
         }
+        catch (FileNotFoundException ex) {
+        	System.out.println ("The file " + fileName + " is not found");
+        	return false;
+        }
+        return true;
+        
+	}
+
+    public void add(String name, String phoneNumber) {
+        directory.add(new BusinessName(name), phoneNumber);
+    }
+
+    public String getPhoneNumber(String name) {
+        return directory.getValue(new BusinessName(name));
+    }
+
+    public void remove(String name) {
+        directory.remove(new BusinessName(name));
+    }
+
+    public String getFullNameAndPhone(String s) {
+        return directory.getValue(new BusinessName(s));
     }
 }
